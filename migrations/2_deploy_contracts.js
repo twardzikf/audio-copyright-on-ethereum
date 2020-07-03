46cbcb321f409af0b934b3de478cb7d5db95749c
Mateusz <m.kedzierski@campus.tu-berlin.de>
2020-07-03 16:53:01 +0200

    cleaned up repo

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Database = artifacts.require('Database')
-const Property = artifacts.require('Property')
+const Database = artifacts.require('idDatabase')
+// const Property = artifacts.require('Property')
 
 module.exports = async (deployer) => {
   const OWNER = '0xA4CA6b32b5093C3d9B0Bee41e62dD9E85aaf9819';
@@ -8,11 +8,12 @@ module.exports = async (deployer) => {
       return Database.new();
     }).then((database) => {
       console.log('database: ' + database.address);
-      return Property.new('string', OWNER, database.address);
-    }).then((property) => {
-      console.log('property: ' + property.address);
-      
+      // return Property.new('string', OWNER, database.address);
     });
+    // .then((property) => {
+    //   console.log('property: ' + property.address);
+      
+    // });
     // TODO deploy Property using address from deployed Database
   } catch (error) {
     console.error(error);
